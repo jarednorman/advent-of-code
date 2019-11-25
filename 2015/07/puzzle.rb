3592378b42057c25f5a98132c0b3025c19dfbe2b
Jared Norman <jared@super.gd>
2019-11-24 18:24:01 -0800

    This doesn't work, try something else

--- 2015/07/puzzle.rb
+++ 2015/07/puzzle.rb
@@ -4,28 +4,74 @@ class PartOne
 
   def solve
     commands.each do |command|
+      binding.pry unless command[:orig] == reconstruct(command)
+
       case command[:operator]
       when :AND, :OR, :RSHIFT, :LSHIFT
-        board[command[:destination]] = [command[:operator], command[:operand1], command[:operand2]]
+        board[command[:destination]] = {ins: [command[:operator], command[:operand1], command[:operand2]]}
       when :NOT
-        board[command[:destination]] = [:NOT, command[:operand1]]
+        board[command[:destination]] = {ins: [:NOT, command[:operand1]]}
       when :CONSTANT
-        board[command[:destination]] = [:CONSTANT, command[:operand1]]
-      when :PASS
-        board[command[:destination]] = [:PASS, command[:operand1]]
+        board[command[:destination]] = {ins: [:CONSTANT, command[:operand1]]}
       else
         raise "SMOOTH OPERATOR: #{command[:operator].inspect}"
       end
     end
-    binding.pry
+
+    result = evaluate(:a)
+    # binding.pry
+    result
   end
 
   private
+
+  def evaluate(wire_name)
+    wire = board[wire_name]
+    ins = wire[:ins]
+
+    wire[:value] ||=
+      case ins.first
+      when :AND
+        value(ins[1]) & value(ins[2])
+      when :OR
+        value(ins[1]) | value(ins[2])
+      when :RSHIFT
+        value(ins[1]) >> value(ins[2])
+      when :LSHIFT
+        value(ins[1]) << value(ins[2])
+      when :NOT
+        ~value(ins[1])
+      when :CONSTANT
+        value(ins[1])
+      else
+        raise "UNEXPECTED OPERATOR: #{ins.first.inspect}"
+      end.clamp(0, 65535)
+
+    wire[:value]
+  end
+
+  # Given either a literal value or wire name, evaluate it.
+  def value(x)
+    return x.clamp(0, 65535) if x.is_a? Integer
+    evaluate(x).clamp(0, 65535)
+  end
 
   def board
     @wires ||= Hash.new
   end
 
+  def reconstruct command
+    case command[:operator]
+    when :AND, :OR, :RSHIFT, :LSHIFT
+      "#{command[:operand1]} #{command[:operator]} #{command[:operand2]} -> #{command[:destination]}"
+    when :NOT
+      "NOT #{command[:operand1]} -> #{command[:destination]}"
+    when :CONSTANT
+      "#{command[:operand1]} -> #{command[:destination]}"
+    else
+      raise "SMOOTH OPERATOR: #{command[:operator].inspect}"
+    end
+  end
 
   def input
     @input ||=
@@ -40,36 +86,40 @@ class PartOne
        if m = /(\w+) (AND|OR|RSHIFT|LSHIFT) (\w+) -> (\w+)/.match(c)
          a, operator, b, dest = *m.captures
          {
+           orig: c,
            operator: operator.to_sym,
-           operand1: a.to_sym,
-           operand2: b,
+           operand1: norm(a),
+           operand2: norm(b),
            destination: dest.to_sym
          }
        elsif m = /NOT (\w+) -> (\w+)/.match(c)
          a, dest = *m.captures
          {
+           orig: c,
            operator: :NOT,
-           operand1: a.to_sym,
-           destination: dest.to_sym
-         }
-       elsif m = /(\d+) -> (\w+)/.match(c)
-         value, dest = *m.captures
-         {
-           operator: :CONSTANT,
-           operand1: value.to_sym,
+           operand1: norm(a),
            destination: dest.to_sym
          }
        elsif m = /(\w+) -> (\w+)/.match(c)
-         source, dest = *m.captures
+         a, dest = *m.captures
          {
-           operator: :PASS,
-           operand1: source.to_sym,
+           orig: c,
+           operator: :CONSTANT,
+           operand1: norm(a),
            destination: dest.to_sym
          }
        else
          raise "BAD COMMAND: #{c.inspect}"
        end
     }
+  end
+
+  def norm a
+    if a.to_i.to_s == a
+      a.to_i
+    else
+      a.to_sym
+    end
   end
 end
 
