29b4ede7d2504bb4ae409c7cf439a17c15527213
Jared Norman <jared@super.gd>
2019-11-24 19:09:54 -0800

    Day 9 part one

---
+++ 2015/09/puzzle.rb
@@ -0,0 +1,43 @@
+class PartOne
+  def initialize
+  end
+
+  def solve
+    input.each do |line|
+      a, b, d = */(\w+) to (\w+) = (\d+)/.match(line).captures
+
+      locations << a
+      locations << b
+
+      distances[Set.new([a, b])] = d.to_i
+    end
+
+    locations.to_a.permutation.map { |locs|
+      locs.each_cons(2).sum { |x| distances[Set.new(x)] }
+    }.min
+  end
+
+  private
+
+  def distances
+    @distances ||= {}
+  end
+
+  def locations
+    @locations ||= Set.new
+  end
+
+  def input
+    @input ||=
+      begin
+        path = File.expand_path(File.dirname(__FILE__))
+        File.read(Pathname.new(path).join("input.txt")).chomp.lines.lazy.map(&:chomp)
+      end
+  end
+end
+
+class PartTwo < PartOne
+  def solve
+    0
+  end
+end
